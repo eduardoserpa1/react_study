d4ffe22f6f2a9b9e3b089b7b48ff04a8f970925a
eduardoserpa1 <dudu.rserpa@gmail.com>
2024-01-31 12:02:06 -0300

    finish 4th class and added dynamic list using states from react

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from '../Input'
 import styled from 'styled-components'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -23,14 +25,44 @@ const Subtitulo = styled.h3`
         margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 function Pesquisa(){
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>   
             <Input
                 placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultado = livros.filter(livro => livro.nome.toLowerCase().includes(textoDigitado.toLowerCase()))
+                    setLivrosPesquisados(resultado)
+                }}
             />
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            )) }
         </PesquisaContainer>
     )
 }
